355326e806440a4fee53567d18606ae4ae5f4744
mqxu <moqi1977@gmail.com>
2022-03-12 10:49:45 +0800

    Vue3基础语法(1)

---
+++ ES6常用语法复习/s.js
@@ -0,0 +1,19 @@
+
+var paramData = {}
+paramData.name = '张三';
+paramData.id = 1;
+paramData.phone = '13988887777';
+
+paramData.bestFriend = {}
+paramData.bestFriend["address"] = '北京';
+paramData.bestFriend["gender"] = '男';
+
+
+
+
+console.log(JSON.stringify(paramData));
+
+
+
+
+
